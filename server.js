8ea9b118e2d6fa4cdb763d3afa5259ea9d68aee2
Roboneet <neethumariyajoy@gmail.com>
2017-07-03 05:54:56 +0530

    basic post request

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const bodyParser = require('body-parser')
+const app = express();
+
 
 app.listen(3000, function(){
 	console.log("listening on port 3000")
@@ -7,4 +9,8 @@ app.listen(3000, function(){
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/new', (req, res) => {
+	console.log('your fav cartoon is :')
 })
